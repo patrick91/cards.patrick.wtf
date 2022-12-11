d19a4031d8510b9ec53f565a3a971941147a71c2
Patrick Arminio <patrick.arminio@gmail.com>
2022-12-11 20:54:59 +0000

    Initial commit

---
+++ pages/api/card.tsx
@@ -0,0 +1,57 @@
+import { ImageResponse } from "@vercel/og";
+
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+// Make sure the font exists in the specified path:
+const font = fetch(
+  new URL("../../assets/SixHandsBlack.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export default async function handler(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  let title = searchParams.get("title");
+
+  if (!title) {
+    title = "Hello World!";
+  } else {
+    title = decodeURIComponent(escape(atob(title)));
+  }
+
+  const fontData = await font;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundColor: "#f8cf7b",
+          height: "100%",
+          width: "100%",
+          fontSize: 100,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontFamily: '"SixHandsBlack"',
+        }}
+      >
+        {title}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      emoji: "twemoji",
+      fonts: [
+        {
+          name: "SixHandsBlack",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
